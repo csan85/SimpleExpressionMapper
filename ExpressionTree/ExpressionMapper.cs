34b336ea98ac9b03b5bdb88f5775716c58356921
csan85 <christian.santarossa@outlook.com>
2015-07-15 21:46:45 +0200

    Semplice mapper con ExpressionTree
    Un semplice mapper tra classi utilizzando gli ExpressionTree

---
+++ ExpressionTree/ExpressionMapper.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace ExpressionTree
+{
+    public static class ExpressionMapper
+    {
+        private static Dictionary<Tuple<Type, Type>, Lazy<Delegate>> mapDelegate = new Dictionary<Tuple<Type, Type>, Lazy<Delegate>>();
+
+        public static void CreateMap<TOrigin, TDestination>()
+            where TOrigin : class
+            where TDestination : class
+        {
+            CreateDelegate(typeof(TOrigin), typeof(TDestination));
+        }
+
+        public static TDestination Map<TOrigin, TDestination>(TOrigin origin)
+            where TOrigin : class
+            where TDestination : class
+        {
+            Lazy<Delegate> @delegate = null;
+            if (mapDelegate.TryGetValue(new Tuple<Type, Type>(typeof(TOrigin), typeof(TDestination)), out @delegate))
+            {
+                return ((Func<TOrigin, TDestination>)@delegate.Value)(origin);
+            }
+
+            return null;
+        }
+
+
+        private static void CreateDelegate(Type origin, Type destination)
+        {
+            Type originTypeToMap = null;
+            Type destinationTypeToMap = null;
+
+            bool isOriginEnumerable = false;
+            bool isDestinationEnumerable = false;
+            if (origin.IsGenericType && destination.IsGenericType)
+            {
+                Type originArgumentType = origin.GenericTypeArguments[0];
+                Type destinationArgumentType = destination.GenericTypeArguments[0];
+
+                isOriginEnumerable = origin.IsAssignableFrom(typeof(List<>).MakeGenericType(originArgumentType));
+                isDestinationEnumerable = destination.IsAssignableFrom(typeof(List<>).MakeGenericType(destinationArgumentType));
+
+                if (isOriginEnumerable && isDestinationEnumerable)
+                {
+                    originTypeToMap = origin.GenericTypeArguments[0];
+                    destinationTypeToMap = destination.GenericTypeArguments[0];
+                }
+            }
+            else
+            {
+                originTypeToMap = origin;
+                destinationTypeToMap = destination;
+            }
+
+            var parameterOrigin = Expression.Parameter(origin, "o");
+
+            Lazy<Delegate> @delegate = null;
+            if (isOriginEnumerable && isDestinationEnumerable)
+            {
+                var expression = CreateForeachExpression(origin, destination, originTypeToMap, destinationTypeToMap, parameterOrigin);
+                Type delegateType = typeof(Func<,>).MakeGenericType(origin, destination);
+
+                Func<Delegate> createFunction = new Func<Delegate>(() =>
+                    Expression.Lambda(delegateType, expression, parameterOrigin).Compile());
+
+                @delegate = new Lazy<Delegate>(createFunction);
+            }
+            else
+            {
+                var expression = CreateMapByBinding(origin, destination, parameterOrigin);
+                Type delegateType = typeof(Func<,>).MakeGenericType(origin, destination);
+
+                Func<Delegate> createFunction = new Func<Delegate>(() =>
+                    Expression.Lambda(delegateType, expression, parameterOrigin).Compile());
+
+                @delegate = new Lazy<Delegate>(createFunction);
+            }
+
+            mapDelegate.Add(new Tuple<Type, Type>(origin, destination), @delegate);
+        }
+        
+        public static Expression CreateMapByBinding(Type origin, Type destination, ParameterExpression parameterOrigin)
+        {
+            Dictionary<string, PropertyInfo> dictionaryInfoOrigine = origin
+                .GetProperties()
+                .Where(p => p.CanRead)
+                .ToDictionary(i => i.Name, i => i);
+
+            IEnumerable<PropertyInfo> infoDestinazione = destination
+                .GetProperties()
+                .Where(p => p.CanWrite);
+
+            List<MemberBinding> bindings = new List<MemberBinding>();
+            foreach (PropertyInfo info in infoDestinazione)
+            {
+                PropertyInfo sourceInfo = null;
+                if (dictionaryInfoOrigine.TryGetValue(info.Name, out sourceInfo) && info.PropertyType == sourceInfo.PropertyType)
+                {
+                    var binding = Expression.Bind(info, Expression.Property(parameterOrigin, sourceInfo));
+                    bindings.Add(binding);
+                }
+                else if (sourceInfo != null && info.PropertyType == typeof(string))
+                {
+                    var property = Expression.Property(parameterOrigin, sourceInfo);
+                    var propertyToString = Expression.Call(property, sourceInfo.PropertyType.GetMethod("ToString", new Type[] { }));
+
+                    var binding = Expression.Bind(info, propertyToString);
+                    bindings.Add(binding);
+                }
+            }
+
+            return Expression.MemberInit(Expression.New(destination), bindings);
+        }
+
+        private static Expression CreateForeachExpression(Type origin, Type destination, Type originTypeToMap, Type destinationTypeToMap,
+                                                          ParameterExpression parameterOrigin)
+        {
+            var index = Expression.Variable(typeof(int), "i");
+            var item = Expression.Variable(originTypeToMap, "item");
+            var itemToAdd = Expression.Variable(destinationTypeToMap, "itemToAdd");
+            var resultList = Expression.Variable(destination, "restultCollection");
+            var list = Expression.Variable(origin, "origin");
+
+            var @break = Expression.Label();
+
+            /*
+             * https://msdn.microsoft.com/en-us/library/dd324074(v=vs.100).aspx
+             * si trova scritto che:
+             *  - Remarks: When the block expression is executed, it returns the value of the last expression in the block.
+             */
+
+            var body = CreateMapByBinding(originTypeToMap, destinationTypeToMap, item);
+
+            var expression = Expression.Block(
+                new ParameterExpression[] { item, index, resultList, itemToAdd, list },
+                Expression.Assign(list, parameterOrigin),
+                Expression.Assign(resultList, Expression.New(destination)),
+                Expression.Assign(index, Expression.Constant(0)),
+                Expression.Loop(
+                    Expression.IfThenElse(
+                        Expression.LessThan(index, Expression.Property(list, origin.GetProperty("Count"))),
+                        Expression.Block(
+                            Expression.Assign(item, Expression.MakeIndex(list, origin.GetProperty("Item"), new Expression[] { index })),
+                            Expression.Assign(itemToAdd, body),
+                            Expression.Call(resultList, destination.GetMethod("Add"), itemToAdd),
+                            Expression.AddAssign(index, Expression.Constant(1))
+                        ),
+                        Expression.Break(@break))
+                , @break),
+                resultList
+            );
+            return expression;
+        }
+    }
+}
---
+++ ExpressionTree/Program.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Linq.Expressions;
+
+namespace ExpressionTree
+{
+    public class SimpleClass
+    {
+        public string Prova1 { get; set; }
+        public int Prova2 { get; set; }
+        public System.DateTime Prova3 { get; set; }
+        public System.Guid Prova4 { get; set; }
+        public decimal Prova5 { get; set; }
+    }
+
+    public class Copia
+    {
+        public string Prova1 { get; set; }
+        public int Prova2 { get; set; }
+        public System.DateTime Prova3 { get; set; }
+        public System.Guid Prova4 { get; set; }
+        public string Prova5 { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MapperUsage();
+            MapperListUsage();
+
+            Performance();
+
+            Console.ReadLine();
+        }
+
+        private static void MapperUsage()
+        {
+            ExpressionMapper.CreateMap<SimpleClass, Copia>();
+
+            SimpleClass simpleClass = new SimpleClass()
+            {
+                Prova1 = "qualcosa",
+                Prova2 = 124,
+                Prova3 = System.DateTime.Today,
+                Prova4 = System.Guid.NewGuid(),
+                Prova5 = 123.567m
+            };
+
+            Copia copia = ExpressionMapper.Map<SimpleClass, Copia>(simpleClass);
+        }
+
+        private static void MapperListUsage()
+        {
+            ExpressionMapper.CreateMap<List<SimpleClass>, List<Copia>>();
+
+            SimpleClass simpleClass = new SimpleClass()
+            {
+                Prova1 = "qualcosa",
+                Prova2 = 124,
+                Prova3 = System.DateTime.Today,
+                Prova4 = System.Guid.NewGuid(),
+                Prova5 = 123.567m
+            };
+
+            List<Copia> copia = ExpressionMapper.Map<List<SimpleClass>, List<Copia>>(new List<SimpleClass>() { simpleClass, new SimpleClass() });
+        }
+
+        private static void Performance()
+        {
+            SimpleClass simpleClass = new SimpleClass()
+            {
+                Prova1 = "qualcosa",
+                Prova2 = 124,
+                Prova3 = System.DateTime.Today,
+                Prova4 = System.Guid.NewGuid(),
+                Prova5 = 123.567m
+            };
+
+            Stopwatch watch = new Stopwatch();
+            Console.WriteLine("---------- Get Properties ----------");
+
+            watch.Restart();
+            Type type = typeof(SimpleClass);
+            PropertyInfo[] propertyInfos = type.GetProperties();
+            for (int i = 0; i < 100000; ++i)
+            {
+                foreach (PropertyInfo info in propertyInfos)
+                {
+                    info.GetValue(simpleClass, null);
+                }
+            }
+            watch.Stop();
+            Console.WriteLine(String.Format("{0} Reflection", watch.Elapsed));
+
+            watch.Restart();
+            type = typeof(SimpleClass);
+            ParameterExpression parameter = Expression.Parameter(type, "c");
+            List<BinaryExpression> assignments = new List<BinaryExpression>();
+            List<ParameterExpression> variables = new List<ParameterExpression>();
+            foreach (PropertyInfo info in type.GetProperties())
+            {
+                ParameterExpression variable = Expression.Variable(info.PropertyType);
+                BinaryExpression assignment = Expression.Assign(variable, Expression.Property(parameter, info));
+
+                variables.Add(variable);
+                assignments.Add(assignment);
+            }
+
+            Action<SimpleClass> action = Expression.Lambda<Action<SimpleClass>>(Expression.Block(variables, assignments), parameter).Compile();
+
+            for (int i = 0; i < 100000; ++i)
+            {
+                action(simpleClass);
+            }
+            watch.Stop();
+            Console.WriteLine(String.Format("{0} Expression", watch.Elapsed));
+
+            watch.Restart();
+            for (int i = 0; i < 100000; ++i)
+            {
+                action(simpleClass);
+            }
+            watch.Stop();
+            Console.WriteLine(String.Format("{0} Created Expression", watch.Elapsed));
+
+            watch.Restart();
+            Action<SimpleClass> simpleAction = (c) =>
+            {
+                string obj1 = c.Prova1;
+                int obj2 = c.Prova2;
+                DateTime obj3 = c.Prova3;
+                Guid obj4 = c.Prova4;
+                decimal obj5 = c.Prova5;
+            };
+            for (int i = 0; i < 100000; ++i)
+            {
+                simpleAction(simpleClass);
+            }
+            watch.Stop();
+            Console.WriteLine(String.Format("{0} Property", watch.Elapsed));
+
+            Console.WriteLine();
+            Console.WriteLine("---------- Constructor ----------");
+
+            watch.Restart();
+            for (int i = 0; i < 100000; ++i)
+            {
+                typeof(SimpleClass).GetConstructor(new Type[] { }).Invoke(new object[] { });
+            }
+            watch.Stop();
+            Console.WriteLine(String.Format("{0} Reflection", watch.Elapsed));
+
+            watch.Restart();
+            for (int i = 0; i < 100000; ++i)
+            {
+                Activator.CreateInstance<SimpleClass>();
+            }
+            watch.Stop();
+            Console.WriteLine(String.Format("{0} Activator", watch.Elapsed));
+
+            watch.Restart();
+            for (int i = 0; i < 100000; ++i)
+            {
+                Func<SimpleClass> function1 = Expression.Lambda<Func<SimpleClass>>(Expression.New(typeof(SimpleClass))).Compile();
+                function1();
+            }
+            watch.Stop();
+            Console.WriteLine(String.Format("{0} Expression sempre generata", watch.Elapsed));
+
+            watch.Restart();
+            Func<SimpleClass> function = Expression.Lambda<Func<SimpleClass>>(Expression.New(typeof(SimpleClass))).Compile();
+            for (int i = 0; i < 100000; ++i)
+            {
+                function();
+            }
+            watch.Stop();
+            Console.WriteLine(String.Format("{0} Expression", watch.Elapsed));
+
+            watch.Restart();
+            for (int i = 0; i < 100000; ++i)
+            {
+                CreateNew<SimpleClass>();
+            }
+            watch.Stop();
+            Console.WriteLine(String.Format("{0} T con new()", watch.Elapsed));
+
+            watch.Restart();
+            for (int i = 0; i < 100000; ++i)
+            {
+                new SimpleClass();
+            }
+            watch.Stop();
+            Console.WriteLine(String.Format("{0} new SimpleClass()", watch.Elapsed));
+        }
+
+        private static T CreateNew<T>() where T : new()
+        {
+            return new T();
+        }
+    }
+}
